688548fd643e635f9cd45e6f1b389c694c77efe8
sonya <jijumbeck@gmail.com>
2024-07-13 17:27:36 +0300

    decompose

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Header';
+import Main from './components/Main';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+    <>
+        <Header />
+        <Main />
+    </>
+);
