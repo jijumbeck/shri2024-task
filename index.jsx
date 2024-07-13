92ca025db480287be542a33f6af86c4f53a432fd
sonya <jijumbeck@gmail.com>
2024-07-13 17:30:48 +0300

    fix adding styles

--- index.jsx
+++ index.jsx
@@ -1,5 +1,8 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import './reset.css';
+import './styles.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
